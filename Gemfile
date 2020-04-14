8b6792ee401343a8402264c93b870301bb6b1b25
Aniruddha Mirajkar <mirajkaraniruddha@gmail.com>
2020-04-14 11:59:20 +0530

    devise added

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-
+gem 'devise'
 # addition added
 # bootstrap
 gem 'bootstrap', '~> 4.4.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-	root 'pages#home'
-	get 'about' => 'pages#about'
-	get 'contactus' => 'pages#contact_us'
+  devise_for :users
+  as :user do
+    get 'signin' => 'devise/sessions#new'
+    get 'signout' => 'devise/sessions#destory'
+    get 'signup' => 'devise/registrations#new'
+  end
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contactus' => 'pages#contact_us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
