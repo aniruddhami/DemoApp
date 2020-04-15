47e22fcb3c16b46b59f7924e1e6239c715d39539
Aniruddha Mirajkar <mirajkaraniruddha@gmail.com>
2020-04-15 15:57:08 +0530

    user chages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :rememberable, :validatable
+
   validates :name, presence: true
   validates :username, uniqueness: true
+
   has_many :tweets
   has_many :items
 end
