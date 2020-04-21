ee38d7ac1710c5a68bc3b2242ba2df68ffeb72b2
Aniruddha Mirajkar <mirajkaraniruddha@gmail.com>
2020-04-21 10:40:19 +0530

    file deleted

--- db/migrate/20200420123744_remove_colunmall_from_picture.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColunmallFromPicture < ActiveRecord::Migration[6.0]
-  def change
-  	remove_column :pictures, :url_file_name
-  	remove_column :pictures, :url_content_type
-  	remove_column :pictures, :url_updated_at
-  	remove_column :pictures, :url_file_size
-  end
-end
